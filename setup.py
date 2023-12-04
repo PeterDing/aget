ca5e992b1eef9af9ab3810ce4592454caa517129
PeterDing <dfhayst@gmail.com>
2023-12-04 18:30:55 +0800

    Use pyproject.toml

--- setup.py
+++ setup.py
@@ -1,43 +1,8 @@
+#!/usr/bin/env python
 
-import sys
+# This is a shim to hopefully allow Github to detect the package, build is done with poetry
 
+import setuptools
 
-try:
-    from setuptools import setup
-except ImportError:
-    # Use distutils.core as a fallback.
-    # We won't be able to build the Wheel file on Windows.
-    from distutils.core import setup
-
-if sys.version_info < (3, 5, 0):
-    raise RuntimeError("aget requires Python 3.5.0+")
-
-version = "0.1.17"
-
-requires = ["mugen"]
-
-setup(
-    name="aget",
-    version=version,
-    author="PeterDing",
-    author_email="dfhasyt@gmail.com",
-    license="Apache 2.0",
-
-    description="",
-    url="http://github.com/PeterDing/aget",
-
-    install_requires=requires,
-
-    classifiers=[
-        "License :: OSI Approved :: Apache Software License",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
-    ],
-
-    packages=["aget"],
-
-    scripts=['bin/aget'],
-
-    include_package_data=True
-)
+if __name__ == "__main__":
+    setuptools.setup(name="aget")
